0635f1c1abcdc3a9c80becda5c260f2d102555fd
Zachary Schroeder <schroza@gmail.com>
2013-02-14 00:53:21 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitewolf::Application.config.secret_token = 'fdaf11261b468eb26f18c3fb7f68395c6c2ded861e44c33d72199397a12ccaa3f7aa3ec664a1cadc475f98e5bf6381737f7e9214be3dc24afa410da94c0897e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bitewolf::Application.config.session_store :cookie_store, key: '_bitewolf_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bitewolf::Application.config.session_store :active_record_store
